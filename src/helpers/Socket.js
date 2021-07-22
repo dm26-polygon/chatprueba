ef519a1dbe78f2c0f0e1ccaf42a42f3f408cc073
luis moreno <dm26.dev@gmail.com>
2021-07-22 11:40:10 -0500

    Nada

--- src/helpers/Socket.js
+++ src/helpers/Socket.js
@@ -1,6 +1,6 @@
 import io from 'socket.io-client';
 
 // let socket = io("https://servernodejuan.herokuapp.com/")
-let socket = io("https://servernodejuan.herokuapp.com/")
+let socket = io("https://deploy2chat.herokuapp.com/")
 
 export default socket
